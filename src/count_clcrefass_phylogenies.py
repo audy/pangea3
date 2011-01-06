6a34622e54d0be59f2611213f17b5656edc618a0
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-01-06 10:51:25 -0500

    init

---
+++ src/count_clcrefass_phylogenies.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# Austin G. Davis-Richardson
+
+# CLC Ref Assembly + RDP => Description, number of reads
+
+# How to:
+
+# Python <1, 0 or 10> <fasta> <clc table>
+# 1  = paired
+# 0  = not paired
+# 10 = both
+
+''' 
+  Print information about each match in an assembly file. The columns are:
+
+    0 Read number
+    1 Read name (enable using the `-n' option)
+    2 Read length
+    3 Read position for alignment start
+    4 Read position for alignment end
+    5 Reference sequence number
+    6 Reference position for alignment start
+    7 Reference position for alignment end
+    8 Whether the read is reversed (0 = no, 1 = yes)
+    9 Number of matches
+    10 Whether the read is paired with the next one (0 = no, 1 = yes) (enable
+                                                          using the `-p' option
+    11 Alignment score (enable using the `-s' option)
+
+'''
+import sys
+from collections import defaultdict
+
+paired = defaultdict(int)
+unpaired = defaultdict(dict)
+
+f_rdp, f_db = sys.argv[1:]
+
+print '==> %s <==' % f_db
+
+with open(f_db) as handle:
+    for line in handle:
+        line = line.split()
+        try:
+            line[10]
+        except:
+            print line
+            quit()
+        if line[10] is '1': # then it's paired
+            paired[int(line[5])] += 1
+            try:
+                handle.next() # dont count twice, skip next line
+            except StopIteration:
+                continue
+        elif line[10] is '0': # then it's not paired
+            current = int(line[5])
+            try:
+                next = int(handle.next().split()[5]) # Go to next line get num
+                if next in unpaired[current]:
+                    unpaired[current][next] += 1
+                else:
+                    unpaired[current][next] = 1
+            except StopIteration:
+                continue
+
+# count totals
+total = 0
+for i in unpaired:
+    for j in unpaired[i]:
+        total += unpaired[i][j]
+print 'unpaired = %s' % total
+
+total = 0
+for i in paired:
+    total += paired[i]
+print '  paired = %s' % total
+
+# load RDP; we need random access!
+rdp, c = {}, 0
+with open(f_rdp) as handle:
+    for line in handle:
+        if line.startswith('>'):
+            c += 1
+            rdp[c] = line[1:-1]
+
+merged_counts = defaultdict(int) # getting uglier
+skipped = 0
+for first in unpaired:
+    for second in unpaired[first]:
+        try:
+            first_name, second_name = rdp[first], rdp[second]
+        except KeyError:
+            skipped += unpaired[first][second]
+            continue
+            
+        first_name = first_name.split(';')
+        second_name = second_name.split(';')
+        consensus = []
+        for f, s in zip(first_name, second_name):
+            if f == s:
+                consensus.append(f)
+            else:
+                merged_counts[';'.join(consensus)] += unpaired[first][second]
+                break
+
+print ' skipped = %s' % skipped
+
+
+with open('%s.unpaired.out' % sys.argv[2], 'w') as handle:
+    for c in merged_counts:
+        print >> handle, '%s, %s' % (c, merged_counts[c])
+    
+with open('%s.paired.out' % sys.argv[2], 'w') as handle:
+    for hit in paired:
+        print >> handle, '%s, %s' % (rdp[hit], paired[hit])
+    
+        
---
+++ src/mgtbl.pl
@@ -0,0 +1,131 @@
+# !/usr/bin/perl -w
+#	
+# megaclustable.pl
+# Written by: David Crabb
+###########################################################################################
+#
+#	Parameters:
+#		-m list of all megaclust output files seperated by spaces
+#		-t level of taxa that megaclust was run for where 0= kingdom and 6= species
+#		-o output table name
+#
+###########################################################################################
+
+use File::Basename;
+
+if($#ARGV < 5)
+{
+	print "Please enter the correct parameters.\n";
+	exit;
+}
+
+#organizes input
+$mIn = "false";
+for($a = 0; $a <= $#ARGV; $a++)
+{
+	if($ARGV[$a] eq "-m")
+	{
+		$mIn = "true";
+	}
+	elsif($ARGV[$a] eq "-o")
+	{
+		$mIn = "false";
+		$a++;
+		$output = $ARGV[$a];
+	}
+	elsif($ARGV[$a] eq "-t")
+	{
+		$mIN = "false";
+		$a++;
+                $taxLevel = $ARGV[$a];
+                if($taxLevel > 10 || $taxLevel < 0)
+		{
+			print "You must enter a number between 0 and 6 for taxonomy level where 0 = kingdom and 6 = species.\n";
+			exit;
+		}
+		$taxLevel = "["."$taxLevel"."]";
+	}
+	elsif($mIn eq "true")
+	{
+		push(@megaclustFile, $ARGV[$a]);
+	}
+}
+
+$numMega = scalar @megaclustFile;
+@table = ();
+@taxa = ();
+$size = 0;
+
+for($b = 0; $b < $numMega; $b++)
+{
+	unless (open(MEGA, $megaclustFile[$b]))       #tries to open file
+
+	{
+		print "Unable to open $megaclustFile[$b]\nMake sure you entered the extension when entering the file name.\n";
+		exit;
+	}
+	@file = ();
+	for($a = 0; $a < $size; $a++)
+	{
+		push(@file, "0");
+	}
+	while($line = <MEGA>)
+	{
+		chomp($line);
+		$loc = index($line, $taxLevel);
+		if($loc > -1)
+		{
+			$found = "false";
+			$loc += 3;
+			$end = index($line, ";", $loc);
+			if($end == -1)
+			{
+				$end = index($line, ",", $loc);
+			}
+			$name = substr($line, $loc, $end - $loc);
+			$numStart = index($line, ",", $end) + 1;
+			#$numEnd = index($line, "\n", $numStart);
+			$num = substr($line, $numStart, length($line) - $numStart);
+			for($a = 0; $a < $size; $a++)
+			{
+				if($taxa[$a] eq "$name")
+				{
+					$found = "true";
+					$file[$a]+= $num;
+				}
+			}
+			
+			if($found eq "false")
+			{
+				push(@taxa, $name);
+				push(@file, $num);
+				$size++;
+			}
+		}
+	}
+	close MEGA;
+	push(@table, [@file]);
+}
+
+unshift(@table, [@taxa]);
+open OUTPUT, ">$output" or die $!;
+for($a = 0; $a < $numMega; $a++)
+{
+        $filename = basename($megaclustFile[$a]);
+        $filename = substr($filename, 0, 9);
+	print OUTPUT "\t$filename";
+}
+for($a = 0; $a < $size; $a++)
+{
+	print OUTPUT "\n";
+	for($b = 0; $b < $numMega + 1; $b++)
+	{
+		if($table[$b][$a] eq "")
+		{
+			$table[$b][$a] = 0;
+		}
+		print OUTPUT "$table[$b][$a]\t";
+	}
+}
+
+
