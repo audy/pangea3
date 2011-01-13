5166458c59f987dbec56765859bbebbc8000cf02
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-01-13 14:56:07 -0500

    shannon diversificiation up in here!

--- src/shannon.pl
+++ src/shannon.pl
@@ -1,15 +1,14 @@
-# !/usr/bin/env perl -w
+# !/usr/bin/perl -w
 #
 # shannon.pl
 # Written by: David Crabb
 # Eric Triplett's Group
 # University of Florida
-# Last Modified: May 25, 2010
+# Last Modified: January 13, 2011
 #######################################################
 #
 # Parameters:
 #   -t hybrid table input file
-#   -n number of samples
 #   -o output name
 #
 #######################################################
@@ -17,55 +16,84 @@
 use Getopt::Std;
 
 my %parameters;
-getopts('t:n:', \%parameters);
+getopts('t:', \%parameters );
 
-$usage = "shannon.pl -t <input>\n";
-unless ($parameters{t}) {
-  print STDERR $usage;
-  exit;
+unless ( $parameters{t} ) {
+    print STDERR
+"Please enter the program with -t hybrid table input file -o output seqeunce file\n";
+    exit;
 }
 
-unless (open(INPUT, $parameters{t})) {
-  print STDERR "Unable to open $parameters{t}\n.";
-  exit;
+unless ( open( INPUT, $parameters{t} ) ) {
+    print STDERR "Unable to open $parameters{t}\n";
+    exit;
 }
 
-print STDERR "SDI: $parameters{t}\n";
-
-@in = <INPUT>;
-$inSize = scalar @in;
-
-$in[0] =~ s/\n//;
-@samples = split(/\t/, $in[0]);
-$num = scalar @samples;
-print "SHANNON:\t"; for ($b=1; $b < $num; $b++) { print "@samples[$b]\t"; } print "\nSHANNON:\t";
-
-for ($b = 1; $b < $num; $b++) {
-
-  @data = ();
-  for ($c = 1; $c < $inSize; $c++) {
-    @line = split(/\t/, $in[$c]);
-    push(@data, $line[$b]);
-
-  }
-  
-  $size = scalar @data;
-  $sum  = 0;
-  $sum2 = 0;
-  for ($a = 0 ;$a < $size; $a++) {
-    $sum += $data[$a];
-  }
-  
-  for ($a = 0; $a < $size; $a++) {
-    if ($data[$a] != 0) {
-      $data[$a] = $data[$a] / $sum;
-      $data[$a] = $data[$a] * log( $data[$a] );
-      $sum2 += $data[$a];
+$line = <INPUT>;
+chomp($line);
+print "SHANNON:";
+if (($line =~ /\r/) && !($line =~ /\n/)) {
+    @input_Lines = split( /\r/, $line );
+    @samples     = split( /\t/, shift @input_Lines );
+    $num         = scalar @samples;
+    foreach (@samples) {
+        push( @matrix, [$_] );
+#        print "$_\t"; # what does this do?
     }
-  }
-  
-  $sum2 = $sum2 * (-1);
-  $sum2 = sprintf("%.3f", $sum2);
-  print "$sum2\t"
+    print "\n";
+    $count = 0;
+    foreach (@input_Lines) {
+        $count++;
+        print "$_\n";
+        chomp($_);
+        @line_split = split( /\t/, $_ );
+        for ( $a = 0 ; $a < $num ; $a++ ) {
+            push @{ $matrix[$a] }, $line_split[$a];
+        }
+    }
+} else {
+    @samples = split( /\t/, $line );
+    $num = scalar @samples;
+    foreach (@samples) {
+        push( @matrix, [$_] );
+        print "$_\t";
+    }
+    print "\n";
+    $count = 0;
+    while ( $line = <INPUT> ) {
+        $count++;
+
+        chomp($line);
+        @line_split = split( /\t/, $line );
+        for ( $a = 0 ; $a < $num ; $a++ ) {
+            push @{ $matrix[$a] }, $line_split[$a];
+        }
+    }
+}
+close INPUT;
+
+push @{ $matrix[0] }, $entry;
+print "SHANNON:";
+for ( $a = 1 ; $a < $num ; $a++ ) {
+    $sum  = 0;
+    $sum2 = 0;
+    for ( $b = 0 ; $b < $count ; $b++ ) {
+        $sum += $matrix[$a][$b];
+    }
+    for ( $b = 0 ; $b < $count ; $b++ ) {
+        if ( $matrix[$a][$b] != 0 ) {
+            $matrix[$a][$b] = $matrix[$a][$b] / $sum;
+            $matrix[$a][$b] =
+              $matrix[$a][$b] * log( $matrix[$a][$b] );
+            $sum2 += $matrix[$a][$b];
+        }
+    }
+    $sum2 = $sum2 * (-1);
+    $sum2 = sprintf( "%.3f", $sum2 );
+    push @{ $matrix[$a] }, $sum2;
+}
+
+for ( $a = 0 ; $a < $num ; $a++ ) {
+    print "$matrix[$a][$count + 1]\t";
 }
 print "\n";
