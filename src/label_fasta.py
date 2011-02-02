e9bd339189781ce4d5b936c29c10d9aafca39ed2
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-02-02 15:35:32 -0500

    taking a break: why is cdhit giving less clusters for less stringency?

--- src/label_fasta.py
+++ src/label_fasta.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
 # outputs a FASTQ file but with its filename in the header (sorta)
+# Also puts paired reads together with their 5' ends touching
+# This is for clustering
 
 import sys
 import os
+from itertools import cycle
 
 h = os.path.basename(sys.argv[1]).split('.')[0]
 
@@ -12,10 +15,18 @@ if len(sys.argv) == 3:
     add = sys.argv[2]
 else:
     add = ''
+    
+c = cycle([0, 1])
+seq = { 0: '', 1: ''}
 
 with open(sys.argv[1]) as handle:
     for line in handle:
         if line.startswith('>'):
-            print '%s:%s:%s' % (line.strip(), h, add)
+            n = c.next()
+            if n == 1:
+                print '%s:%s:%s' % (line.strip(), h, add)
         else:
-            print line.strip()
+            seq[n] += line.strip()
+            if n == 1:
+                print '%s%s' % (seq[1][::-1], seq[0])
+                seq = { 0: '', 1: ''}
