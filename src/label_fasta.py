2b3e91ff12d56ede843d09b847d5b13fac56086e
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-02-02 11:49:19 -0500

    refactored, implemented logging, label unassembled reads, clustering

---
+++ src/label_fasta.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# outputs a FASTQ file but with its filename in the header (sorta)
+
+import sys
+import os
+
+h = os.path.basename(sys.argv[1]).split('.')[0]
+
+# Optionally add another label
+if len(sys.argv) == 3:
+    add = sys.argv[2]
+else:
+    add = ''
+
+with open(sys.argv[1]) as handle:
+    for line in handle:
+        if line.startswith('>'):
+            print '%s:%s:%s' % (line.strip(), h, add)
+        else:
+            print line.strip()
