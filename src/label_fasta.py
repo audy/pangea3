796aa9cbe1335da8d900bde3fbafbcb8eaedd13d
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-02-05 18:42:50 -0500

    takes input from STDOUT now

--- src/label_fasta.py
+++ src/label_fasta.py
@@ -3,6 +3,7 @@
 # outputs a FASTQ file but with its filename in the header (sorta)
 # Also puts paired reads together with their 5' ends touching
 # This is for clustering
+# Takes input from STDIN
 
 import sys
 import os
@@ -11,22 +12,21 @@ from itertools import cycle
 h = os.path.basename(sys.argv[1]).split('.')[0]
 
 # Optionally add another label
-if len(sys.argv) == 3:
-    add = sys.argv[2]
+if len(sys.argv) == 2:
+    add = sys.argv[1]
 else:
     add = ''
     
 c = cycle([0, 1])
 seq = { 0: '', 1: ''}
 
-with open(sys.argv[1]) as handle:
-    for line in handle:
-        if line.startswith('>'):
-            n = c.next()
-            if n == 1:
-                print '%s:%s:%s' % (line.strip(), h, add)
-        else:
-            seq[n] += line.strip()
-            if n == 1:
-                print '%s%s' % (seq[1][::-1], seq[0])
-                seq = { 0: '', 1: ''}
+for line in sys.stdin:
+    if line.startswith('>'):
+        n = c.next()
+        if n == 1:
+            print '%s:%s:%s' % (line.strip(), h, add)
+    else:
+        seq[n] += line.strip()
+        if n == 1:
+            print '%s%s' % (seq[1][::-1], seq[0])
+            seq = { 0: '', 1: ''}
