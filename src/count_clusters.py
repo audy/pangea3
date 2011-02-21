e89fc4db9e003165ee75f2a00c35a57350a643e0
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-02-21 16:03:13 -0500

    works. makes clusters

--- src/count_clusters.py
+++ src/count_clusters.py
@@ -1,16 +1,56 @@
-#!/usr/bin/env python
-from collections import defaultdict
 import sys
+from collections import defaultdict
+from pprint import pprint
+clust_file = sys.argv[1]
+#fasta_file = sys.argv[2]
+cutoff = int(sys.argv[2])
 
-cluster_file, minimum = sys.argv[1], int(sys.argv[2])
+# Read CLUST File
 
-counts = defaultdict(int)
+counts = {}
 
-with open(cluster_file) as handle:
+with open(clust_file) as handle:
     for line in handle:
-        line = line.split()
-        counts[int(line[0])] += 1
-        
-for count in sorted(counts):
-    if counts[count] >= minimum:
-        print "cluster_%s, %s" % (count, counts[count])
+        if line.startswith('>'):
+            cluster = int(line[1:-1].split()[1])
+
+        else:
+            header = line.split()[2].lstrip('>').rstrip('.')
+            barcode, read = header.split(':')
+            barcode = int(barcode)
+            if cluster in counts:
+                if barcode in counts[cluster]:
+                    counts[cluster][barcode] += 1
+                else:
+                    counts[cluster][barcode] = 1
+            else:
+                counts[cluster] = {}
+                counts[cluster][barcode] = 1
+
+# Print table headers
+
+barcodes = sorted(max(counts.values(), key=len).keys())
+
+def print_headers():
+    
+    print "-\t",
+    for barcode in barcodes:
+        print "%s\t" % barcode,
+    print ''
+    
+print_headers
+
+# Print table values
+
+for cluster in counts:
+    if sum(counts[cluster]) < cutoff:
+        continued
+    print "%s\t" % cluster,
+    for barcode in barcodes:
+        try:
+            val = counts[cluster][barcode]
+        except KeyError:
+            val = '0'
+        print "%s\t" % val,
+    print ''
+print ''
