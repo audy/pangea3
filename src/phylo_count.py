79f79c751dea82a6e7c76bfb2db5ad540df53959
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-04-20 11:19:35 -0400

    make much faster

--- src/phylo_count.py
+++ src/phylo_count.py
@@ -30,49 +30,12 @@
 '''
 import sys
 from collections import defaultdict
+from glob import glob
 
 paired = defaultdict(int)
 unpaired = defaultdict(dict)
 
-f_rdp, f_db = sys.argv[1:]
-
-print '==> %s <==' % f_db
-
-with open(f_db) as handle:
-    for line in handle:
-        line = line.split()
-        try:
-            line[10]
-        except:
-            print line
-        if line[10] is '1': # then it's paired
-            paired[int(line[5])] += 1
-            try:
-                handle.next() # dont count twice, skip next line
-            except StopIteration:
-                continue
-        elif line[10] is '0': # then it's not paired
-            current = int(line[5])
-            try:
-                next = int(handle.next().split()[5]) # Go to next line get num
-                if next in unpaired[current]:
-                    unpaired[current][next] += 1
-                else:
-                    unpaired[current][next] = 1
-            except StopIteration:
-                continue
-
-# count totals
-total = 0
-for i in unpaired:
-    for j in unpaired[i]:
-        total += unpaired[i][j]
-print 'unpaired = %s' % total
-
-total = 0
-for i in paired:
-    total += paired[i]
-print '  paired = %s' % total
+f_rdp, prefix = sys.argv[1:]
 
 # load RDP; we need random access!
 rdp, c = {}, 0
@@ -82,32 +45,75 @@ with open(f_rdp) as handle:
             rdp[c] = line[1:-1]
             c += 1
 
-merged_counts = defaultdict(int) # getting uglier
-skipped = 0
-for first in unpaired:
-    for second in unpaired[first]:
-        try:
-            first_name, second_name = rdp[first], rdp[second]
-        except KeyError:
-            skipped += unpaired[first][second]
-            continue
-            
-        first_name = first_name.split(';')
-        second_name = second_name.split(';')
-        consensus = []
-        for f, s in zip(first_name, second_name):
-            if f == s:
-                consensus.append(f)
-            else:
-                merged_counts[';'.join(consensus)] += unpaired[first][second]
-                break
+for f_db in glob(prefix):
 
-print ' skipped = %s' % skipped
+    print '==> %s <==' % f_db
 
-with open('%s.unpaired.out' % sys.argv[2], 'w') as handle:
-    for c in merged_counts:
-        print >> handle, '%s, %s' % (c.replace(',','_'), merged_counts[c])
-    
-with open('%s.paired.out' % sys.argv[2], 'w') as handle:
-    for hit in paired:
-        print >> handle, '%s, %s' % (rdp[hit].replace(',','_'), paired[hit])
+    with open(f_db) as handle:
+        for line in handle:
+            line = line.split()
+            try:
+                line[10]
+            except:
+                print line
+            if line[10] is '1': # then it's paired
+                paired[int(line[5])] += 1
+                try:
+                    handle.next() # dont count twice, skip next line
+                except StopIteration:
+                    continue
+            elif line[10] is '0': # then it's not paired
+                current = int(line[5])
+                try:
+                    next = int(handle.next().split()[5]) # Go to next line get num
+                    if next in unpaired[current]:
+                        unpaired[current][next] += 1
+                    else:
+                        unpaired[current][next] = 1
+                except StopIteration:
+                    continue
+
+    # count totals
+    total = 0
+    for i in unpaired:
+        for j in unpaired[i]:
+            total += unpaired[i][j]
+    print 'unpaired = %s' % total
+
+    total = 0
+    for i in paired:
+        total += paired[i]
+    print '  paired = %s' % total
+
+    merged_counts = defaultdict(int) # getting uglier
+    skipped = 0
+    for first in unpaired:
+        for second in unpaired[first]:
+            try:
+                first_name, second_name = rdp[first], rdp[second]
+            except KeyError:
+                skipped += unpaired[first][second]
+                continue
+
+            first_name = first_name.split(';')
+            second_name = second_name.split(';')
+            consensus = []
+            for f, s in zip(first_name, second_name):
+                if f == s:
+                    consensus.append(f)
+                else:
+                    merged_counts[';'.join(consensus)] += unpaired[first][second]
+                    break
+
+    print ' skipped = %s' % skipped
+
+    with open('%s.unpaired.out' % f_db, 'w') as handle:
+        for c in merged_counts:
+            print >> handle, '%s, %s' % (c.replace(',','_'), merged_counts[c])
+
+    with open('%s.paired.out' % f_db, 'w') as handle:
+        for hit in paired:
+            print >> handle, '%s, %s' % (rdp[hit].replace(',','_'), paired[hit])
+
+
+
