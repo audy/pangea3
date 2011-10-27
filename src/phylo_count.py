762e200deac0cdb2a944bf7f87facfd0253e7669
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-10-27 12:12:51 -0400

    more stricter

--- src/phylo_count.py
+++ src/phylo_count.py
@@ -5,10 +5,7 @@
 
 # How to:
 
-# Python <1, 0 or 10> <fasta> <clc table>
-# 1  = paired
-# 0  = not paired
-# 10 = both
+# Python <fasta> <clc table>
 
 ''' 
   Print information about each match in an assembly file. The columns are:
@@ -32,10 +29,11 @@ import sys
 from collections import defaultdict
 from glob import glob
 
-paired = defaultdict(int)
-unpaired = defaultdict(dict)
-
+# rdp file, prefix to tables
 f_rdp, prefix = sys.argv[1:]
+
+# counts
+paired_matches = defaultdict(int)
 
 # load RDP; we need random access!
 rdp, c = {}, 0
@@ -44,6 +42,20 @@ with open(f_rdp) as handle:
         if line.startswith('>'):
             rdp[c] = line[1:-1]
             c += 1
+rdp[-1] = 'UNCLASSIFIED'
+
+def compute_lca(a, b):
+    ''' compute the last common ancestor given two strings '''
+    first_name = a.split(';')
+    second_name = b.split(';')
+    consensus = []
+
+    for f, s in zip(first_name, second_name):
+        if f == s:
+            consensus.append(f)
+        else:
+            break
+    return ';'.join(consensus)
 
 for f_db in glob(prefix):
 
@@ -52,68 +64,21 @@ for f_db in glob(prefix):
     with open(f_db) as handle:
         for line in handle:
             line = line.split()
-            try:
-                line[10]
-            except:
-                print line
-            if line[10] is '1': # then it's paired
-                paired[int(line[5])] += 1
-                try:
-                    handle.next() # dont count twice, skip next line
-                except StopIteration:
-                    continue
-            elif line[10] is '0': # then it's not paired
-                current = int(line[5])
-                try:
-                    next = int(handle.next().split()[5]) # Go to next line get num
-                    if next in unpaired[current]:
-                        unpaired[current][next] += 1
-                    else:
-                        unpaired[current][next] = 1
-                except StopIteration:
-                    continue
+            next_line = handle.next().split()
 
-    # count totals
-    total = 0
-    for i in unpaired:
-        for j in unpaired[i]:
-            total += unpaired[i][j]
-    print 'unpaired = %s' % total
+            # get RDP record numbers
+            a_no = int(line[5])
+            b_no = int(next_line[5])
 
-    total = 0
-    for i in paired:
-        total += paired[i]
-    print '  paired = %s' % total
+            # get names
+            a_name = rdp[a_no]
+            b_name = rdp[b_no]
 
-    merged_counts = defaultdict(int) # getting uglier
-    skipped = 0
-    for first in unpaired:
-        for second in unpaired[first]:
-            try:
-                first_name, second_name = rdp[first], rdp[second]
-            except KeyError:
-                skipped += unpaired[first][second]
-                continue
+            # get last common ancestor
+            lca = compute_lca(a_name, b_name)
 
-            first_name = first_name.split(';')
-            second_name = second_name.split(';')
-            consensus = []
-            for f, s in zip(first_name, second_name):
-                if f == s:
-                    consensus.append(f)
-                else:
-                    merged_counts[';'.join(consensus)] += unpaired[first][second]
-                    break
-
-    print ' skipped = %s' % skipped
-
-    with open('%s.unpaired.out' % f_db, 'w') as handle:
-        for c in merged_counts:
-            print >> handle, '%s, %s' % (c.replace(',','_'), merged_counts[c])
+            paired_matches[lca] += 1
 
     with open('%s.paired.out' % f_db, 'w') as handle:
-        for hit in paired:
-            print >> handle, '%s, %s' % (rdp[hit].replace(',','_'), paired[hit])
-
-
-
+        for c in paired_matches:
+            print >> handle, "%s, %s" % (c.replace(',', '_'), paired_matches[c])
